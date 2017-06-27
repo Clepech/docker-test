bf5dab3503ca4131f24f311aebd0b20b53df721a
Your Name <you@example.com>
2017-06-27 14:14:10 +0000

    add Bender wise saying

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
  System.out.println("Hello World");
  System.out.println("Hello Mars");
  System.out.println("Hello Alpha Centauri");
+ System.out.println("KILL ALL HUMANS");
  }
 }
